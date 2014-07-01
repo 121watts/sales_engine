1fc65cb4e57f867762bcc9495cff0214a28a7b3a
Andrew Watkins <andrew.watkinz@gmail.com>
2014-07-01 14:03:51 -0600

    adds customer and customer test

---
+++ test/customer_test.rb
@@ -0,0 +1,25 @@
+require_relative 'test_helper'
+require_relative '../lib/customer'
+
+class CustomerTest < Minitest::Test
+
+  def data
+    {
+      id:         '1',
+      first_name: 'Duder',
+      last_name:  'McDuff',
+      created_at: '2012-03-27 14:54:09 UTC',
+      updated_at: '2012-03-27 14:54:09 UTC'
+    }
+  end
+
+  def test_it_has_attributes
+    customer = Customer.new(data)
+    assert data[:id],         customer.id
+    assert data[:first_name], customer.first_name
+    assert data[:last_name],  customer.last_name
+    assert data[:created_at], customer.created_at
+    assert data[:updated_at], customer.updated_at
+  end
+
+end
