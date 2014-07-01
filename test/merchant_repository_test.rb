be23295d3179806898a69abb5e3d319c6f41f8dc
Andrew Watkins <andrew.watkinz@gmail.com>
2014-07-01 15:13:51 -0600

    adds finds merchants with dirty data

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -11,16 +11,22 @@ class  MerchantRepositoryTest<Minitest::Test
   def test_it_builds_merchants
     repo = MerchantRepository.new
     repo.load('./test/fixtures/small_merchants.csv')
-    repo.build_merchants
-    assert_equal 10, repo.merchants.count
+    assert_equal 10, repo.build_merchants.count
   end
 
-
-  def test_has_merchants
+  def test_find_merchants_by_name
     merchants = MerchantRepository.new
     merchants.load('./test/fixtures/small_merchants.csv')
     names = merchants.find_by_name("Williamson Group")
     assert_equal 2, names.count
   end
+
+  def test_find_merchants_with_dirty_data
+    merchants = MerchantRepository.new
+    merchants.load('./test/fixtures/small_merchants.csv')
+    names = merchants.find_all_by_name("Klein, Rempel and Jones")
+    assert_equal 1, names.count
+  end
+
 
 end
