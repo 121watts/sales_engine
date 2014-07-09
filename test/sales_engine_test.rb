bdc5e4a8124153d72ab0ab482f855e28ccd0ed6f
Andrew Watkins <andrew.watkinz@gmail.com>
2014-07-09 09:21:05 -0600

    fixes sales engine tests

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -5,12 +5,8 @@ class SalesEngineTest < Minitest::Test
 
     attr_reader :engine
 
-    def setup
+    def test_randomness
       engine = SalesEngine.new(true)
-      engine.startup
-    end
-
-    def test_merchant_randomness
       engine.startup
       merchant1 = engine.merchant_repository.random
       merchant2 = engine.merchant_repository.random
@@ -20,18 +16,34 @@ class SalesEngineTest < Minitest::Test
 
     def test_can_find_multiple_records
       engine = SalesEngine.new(true)
-
+      engine.startup
       merchants = engine.merchant_repository.find_all_by_name "Williamson Group"
       assert_equal 2, merchants.count
     end
 
-    def test_merchant_relationships
+    def test_merchant_item_relationships
       engine = SalesEngine.new(true)
+      engine.startup
       merchants = engine.merchant_repository
       merchant  = merchants.find_by_name("Klein, Rempel and Jones")
       assert_equal 2, merchant.id
       items     = engine.item_repository.find_all_by_merchant_id(merchant.id)
       assert items.count > 30
     end
+
+    def test_includes_known_item
+      engine = SalesEngine.new(true)
+      engine.startup
+      item = engine.item_repository.find_by_name('Item Consequatur Eius')
+      refute item.nil?
+    end
+
+    def test_merchant_invoices
+      engine = SalesEngine.new(true)
+      engine.startup
+      invoices = engine.invoice_repository
+      invoices.find_by_merchant_id()
+    end
+
 
 end
