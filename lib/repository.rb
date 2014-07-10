0b07bdfeaa6115756fa644d96578e29c0c227981
Andrew Watkins <andrew.watkinz@gmail.com>
2014-07-10 08:40:56 -0600

    line cleanup

--- lib/repository.rb
+++ lib/repository.rb
@@ -10,7 +10,8 @@ class Repository
 
   def initialize(filename)
     class_name = Kernel.const_get(self.class.to_s.gsub("Repository", ""))
-    @objects = CSV.open(filename, headers: true, header_converters: :symbol).collect do |row|
+    @objects   = CSV.open(filename,
+      headers: true, header_converters: :symbol).collect do |row|
       class_name.new(row)
     end
   end
